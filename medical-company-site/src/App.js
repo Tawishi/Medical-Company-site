356178c3b60148829c9a1ca4b446b856d6f0f82c
Tawishi <tawishisharma1@gmail.com>
2022-08-05 22:29:14 +0530

    Flex fixes

--- medical-company-site/src/App.js
+++ medical-company-site/src/App.js
@@ -1,24 +1,26 @@
-
-import p1 from "./images/p1.jpg"
-import p2 from "./images/p2.jpg"
-import p3 from "./images/p3.jpg"
-import p4 from "./images/p4.jpg"
-import p5 from "./images/p5.jpg"
 import SearchBar from './components/SearchBar';
 import Card from './components/Card';
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Cart from './pages/Cart';
+import NavBar from './components/NavBar';
+import Footer from './components/Footer';
+import CustomerStats from './components/CustomerStats';
 
 window.addEventListener("scroll", () => {
   var header = document.querySelector("header");
   header.classList.toggle("sticky", window.scrollY > 0)
 })
 
+
+
 function App() {
   return (
     <div>
+      <NavBar />
+      <CustomerStats />
       <SearchBar />
-      <Cart/>
+      <Cart />
+      <Footer />
     </div >
   );
 }
--- medical-company-site/src/index.js
+++ medical-company-site/src/index.js
@@ -2,18 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import NavBar from './components/NavBar';
-import Footer from './components/Footer';
-import CustomerStats from './components/CustomerStats';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <NavBar />
-    <CustomerStats/>
-    <App/>
-    <Footer/>
+
+    <App />
+
   </React.StrictMode>
 );
 
